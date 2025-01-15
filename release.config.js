6ae900fc5b66c020d1b615a6f1dbabfd1a6adfab
Tonelli Luca <luca.tonelli11@studio.unibo.it>
2025-01-15 15:36:26 +0100

    ci: fix semathic release

--- release.config.js
+++ release.config.js
@@ -1,6 +1,26 @@
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
-    "@semantic-release/github",
+
     "@semantic-release/git",
-)
-module.exports = config
+    "@semantic-release/github",
+    [
+        "@semantic-release/exec",
+        {
+            // Pre-configured for version management
+            prepareCmd: 'echo "Preparing version ${nextRelease.version}"',
+            publishCmd: 'echo "Publishing version ${nextRelease.version}"',
+            // Status tracking for GitHub Actions
+            successCmd: 'echo "::set-output name=release_created::true"',
+            failCmd: 'echo "::set-output name=release_created::false"'
+        }
+    ]
+);
+
+// Already includes:
+// - @semantic-release/commit-analyzer
+// - @semantic-release/release-notes-generator
+// - @semantic-release/changelog
+
+module.exports = config;
+
+
