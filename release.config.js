e1ab2884eb1ad6f399a325e835bd6e522facc508
Alessandro Becci <trofui@gmail.com>
2025-01-15 15:27:39 +0100

    ci: release

--- release.config.js
+++ release.config.js
@@ -1,16 +1,6 @@
 var config = require('semantic-release-preconfigured-conventional-commits');
-
 config.plugins.push(
-  "@semantic-release/github",
-  "@semantic-release/git",
-  [
-    "@semantic-release/exec",
-    {
-      successCmd: 'echo "Release succeeded!"',
-      failCmd: 'echo "Release failed!"',
-    },
-  ]
-);
-
-module.exports = config;
-
+    "@semantic-release/github",
+    "@semantic-release/git",
+)
+module.exports = config
